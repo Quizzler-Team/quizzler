1e36dd3bf75b1718f173ae613ce3cfb47efc3afb
VenomVelvet <venomvelvet@googlemail.com>
2025-11-11 11:37:15 +0100

    new field

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False, nullable=False)
 
     def __repr__(self):
         return f"<User {self.id} ({self.name})>"
